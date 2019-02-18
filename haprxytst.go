dc395cd299bf0842743f22cf31453c7a08e20c75
Alexei Tighineanu <alexeitighineanu@Imba-MacBookPro.fritz.box>
2019-02-18 12:33:55 +0100

    new checks

--- haprxytst.go
+++ haprxytst.go
@@ -7,11 +7,40 @@ import (
 )
 
 var command1 = []string{"crm", "status"}
+var Nodes, Unregistered []string
+
+func Regchecker(command2 string, ip string) int {
+	//-----------checking if Nodes are registered on sbd:
+
+	checker := strings.Split(ssher.SSH("root", ip, command2, "string"), "\n")
+	fmt.Println(checker)
+	var registered int
+	Unregistered := Nodes
+	for i := 0; i < len(checker); i++ {
+		for j := 0; j < len(Nodes); j++ {
+			if strings.Contains(checker[i], Nodes[j]) {
+				registered += 1
+				for k := 0; k < len(Unregistered); k++ {
+					if Nodes[j] == Unregistered[k] {
+						copy(Unregistered[k:], Unregistered[k+1:]) // Shift Unregistered[k+1:] left one index.
+						Unregistered[len(Unregistered)-1] = ""     // Erase last element (write zero value).
+						Unregistered = Unregistered[:len(Unregistered)-1]
+					}
+				}
+			}
+		}
+	}
+	return registered
+}
+
+//func haproxytester(command4 string, ip string) {
+//	checker := strings.Split(ssher.SSH("root", ip, command2, "string"), "\n")
+//}
 
 func main() {
 	ip := "10.160.66.220"
 	var sbddevice string
-	var nodes []string
+
 	//-----running crm status to check if nodes online
 	checker := strings.Split(ssher.SSH("root", ip, "crm status", "print"), "\n")
 	for i := 0; i < len(checker); i++ {
@@ -23,9 +52,9 @@ func main() {
 					//-------------remembering the node titles "1" "2" "3" for further testing into array "nodes"
 					for k := 1; k <= 3; k++ {
 						if line[j+k] != "]" {
-							nodes = append(nodes, line[j+k])
-							if len(nodes) > 2 {
-								fmt.Printf("a. Three-node cluster set up and running... \n %v\n", nodes)
+							Nodes = append(Nodes, line[j+k])
+							if len(Nodes) > 2 {
+								fmt.Printf("a. Three-node cluster set up and running... \n %v\n", Nodes)
 							}
 						}
 					}
@@ -35,9 +64,9 @@ func main() {
 
 		//---------checking if stonith-sbd runs properly on ANY of the nodes
 		if strings.Contains(checker[i], "stonith-sbd") && strings.Contains(checker[i], "Started") {
-			for k := 0; k < len(nodes); k++ {
-				if strings.Contains(checker[i], nodes[k]) {
-					fmt.Printf("\nb. Stonith-SBD runs properly on one of the nodes: \n%v (node name)\n", nodes[k])
+			for k := 0; k < len(Nodes); k++ {
+				if strings.Contains(checker[i], Nodes[k]) {
+					fmt.Printf("\nb. Stonith-SBD runs properly on one of the nodes: \n%v (node name)\n", Nodes[k])
 				}
 			}
 		}
@@ -58,9 +87,24 @@ func main() {
 		}
 
 	}
-	//-----------checking if Nodes are registered on sbd:
-	command2 := "sbd -d " + sbddevice + " list"
-	checker = strings.Split(ssher.SSH("root", ip, command2, "string"), "\n")
-	for 
 
+	command2 := "sbd -d " + sbddevice + " list"
+	registered := Regchecker(command2, ip)
+
+	//----------if not all three nodes are registered to the SBD ---> the program does it "manually" by allocating slot for each node
+	if registered <= 3 {
+		fmt.Printf("\nd. Not all nodes registered, performing node registering \"manually\"...\nhere is the list of unregistered nodes: %v\n", Unregistered)
+		for i := 0; i < len(Unregistered); i++ {
+			command3 := "sbd -d " + sbddevice + " allocate " + Unregistered[i]
+			ssher.SSH("root", ip, command3, "print")
+		}
+		registered = Regchecker(command2, ip)
+	} else {
+		fmt.Println("\ne. All nodes well-registered on running SBD and ready!\n")
+	}
+
+	//------checking if haproxy installed....
+	command4 := "rpm -qi haproxy"
+	ssher.SSH("root", ip, command4, "print")
+	// TO DO... see why
 }
